1e2615c5b4633ae81a0f914d4e4984fa448ffbc2
Brett Wise <brettdwise@gmail.com>
2015-03-05 02:46:48 -0500

    Moved bower install for js and sass so that I could remove folders that my new js task copies files around. It's a dirty hack until I learn browserify.

--- gogo-static-site.sh
+++ gogo-static-site.sh
@@ -7,6 +7,14 @@ echo "Cool hang tight. This might take a few seconds."
 echo "Folder renamed."
 echo -ne '\n' 'build' '\n' 'y' '\n' '404.html' '\n' 'y' '\n' | divshot init
 echo "divshot site initialized"
+(cd src/sass;bower install)
+echo "Sass related Bower packages installed."
+(cd src/js;bower install)
+echo "JS related Bower packages installed."
+rm -rf src/js/vendor/modernizr/feature-detects
+rm -rf src/js/vendor/modernizr/media
+rm -rf src/js/vendor/modernizr/test
+rm -rf src/js/vendor/instantclick/tests
 rm -rf .git
 echo "Old git repo removed."
 git init
@@ -21,9 +29,5 @@ git remote add origin git@github.com:brettwise/$sitename.git
 git push -u origin master
 npm install
 echo "npm packages installed."
-(cd src/sass;bower install)
-echo "Sass related Bower packages installed."
-(cd src/js;bower install)
-echo "JS related Bower packages installed."
 subl .
 gulp
